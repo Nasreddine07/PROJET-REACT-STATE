895f7aa10083105050ea17c1124229ec4c7400f1
Nasreddine07 <115669950+Nasreddine07@users.noreply.github.com>
2022-12-26 19:33:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Timer from "./Component/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "Nasreddine Mrabet",
+      Bio: "I am a Full Stack JS Student",
+      imgSrc:
+        "https://www.zdnet.com/a/img/resize/adcb2eb2cc3f5562f7e80931308e2a08b01947ce/2019/08/19/4a663776-f4a9-4f89-9bee-2d07ee052f5b/istock-1147195672-11.jpg?auto=webp&fit=crop&height=900&width=1200",
+      profession: "web developer",
+      show: false,
+    };
+  }
+  ToggleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.show && (
+          <>
+            <h1>My name is {this.state.fullName}</h1>
+            <h2>{this.state.Bio}</h2>
+            <img
+              src={this.state.imgSrc}
+              style={{ width: "23rem" }}
+              alt="react"
+            />
+            <p>{this.state.profession}</p>
+            <h2>MY PROFILE</h2>
+          </>
+        )}
+
+        {this.state.show && <Timer />}
+        <button onClick={this.ToggleShow}>Show profile</button>
+      </div>
+    );
+  }
 }
-
 export default App;
